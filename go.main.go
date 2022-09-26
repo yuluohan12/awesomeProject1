88ed2ca591d8e6956158236da23bd380b0b600ba
unknown <1802041608@qq.com>
2022-09-26 18:20:05 +0800

    Initial commit

---
+++ go.main.go
@@ -0,0 +1,32 @@
+/*package main
+
+import "fmt"
+
+func main() {
+	var count int = 1
+	var flag bool
+	for count < 100 {
+		count++
+		flag = true
+		for temp := 2; temp < count; temp++ {
+			if count%temp == 0 {
+				flag = false
+			}
+		}
+		if flag == true {
+			fmt.Printf("%d是素数\n", count)
+		} else {
+			continue
+		}
+
+	}
+
+}*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello,GeeK&Bee")
+}
